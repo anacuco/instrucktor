667bc7d15462256495336c4ffc728b4f86b54720
Markus J Doetsch <git@the-enemy.org>
2017-03-31 23:38:50 +0200

    INSTRUCKTOR add animation test

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,43 @@
 /**
  * Created by ana on 31/03/17.
  */
+
+var game = {
+  init: function () {
+    game.layer = document.querySelector('#layer-0');
+    game.currentOffset = game.layer.offsetLeft;
+  },
+
+  placePosts: function() {
+    var postTemplate = document.querySelector('#templates .post');
+    var length = game.layer.offsetWidth;
+
+    for (var i = 0; i < length; i += 50) {
+
+      var post = postTemplate.cloneNode();
+
+      post.setAttribute('style', 'left: ' + i + 'px');
+
+      game.layer.appendChild(post);
+    }
+  },
+
+  layer: null,
+  currentOffset: null,
+
+  scroll: function (amount) {
+    var newOffset = game.currentOffset + amount;
+    game.layer.setAttribute('style', 'left: ' + newOffset + 'px');
+    game.currentOffset = newOffset;
+  }
+};
+
+window.addEventListener('DOMContentLoaded', function () {
+  game.init();
+  game.placePosts();
+
+
+  var animate = setInterval(function () {
+    game.scroll(1);
+  }, 16);
+});
